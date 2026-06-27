9e10bf95c84072c21542c73b92ad2a343fc0e48c
architareddy <architareddy05@gmail.com>
2026-06-27 12:34:32 +0530

    Added week 1 module 4 AAA pattern exercise

---
+++ week 1/module 4/AAA pattern/Exercise4_AAA/src/test/java/cognizant/CalculatorTest.java
@@ -0,0 +1,38 @@
+package cognizant;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up test...");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up after test...");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int num1 = 10;
+        int num2 = 20;
+
+        // Act
+        int result = calculator.add(num1, num2);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
