4f9225367468f9a733733fe6c75f651455d5459d
architareddy <architareddy05@gmail.com>
2026-06-30 19:38:11 +0530

    Added week 1 module 4 exercise- SLF4j logging

---
+++ week 1/module 4/SLF4J/SLF4J_logging/src/main/java/cognizant/Loggingexample.java
@@ -0,0 +1,18 @@
+package cognizant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Loggingexample {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(Loggingexample.class);
+
+    public static void main(String[] args) {
+
+        logger.error("This is an error message");
+
+        logger.warn("This is a warning message");
+
+    }
+}
